066558a4256617ea49e92571bc6a152c39274f46
xming13 <xiaoming33@gmail.com>
2015-08-17 22:36:26 +0800

    Style fix

--- js/pacman.js
+++ js/pacman.js
@@ -5,11 +5,14 @@ $(function () {
     var mainTimeline = new TimelineMax({paused: true});
     var pacmanTimeline = new TimelineMax();
 
-    var timePerDot = .6;
+    var monsterAfterRule = CSSRulePlugin.getRule(".monster:after");
+    console.log(monsterAfterRule);
+    var timePerDot = .5;
     pacmanTimeline.addLabel("pacman")
-        .to(".characters", timePerDot * 15, { left: "100%", ease: Linear.easeNone }, "pacman")
-        .to(".pacman .top", timePerDot / 2, {rotation: -45, repeat: 20, yoyo: true }, "pacman")
-        .to(".pacman .bottom", timePerDot / 2, { rotation: 45, repeat: 20, yoyo: true }, "pacman")
+        .to(".characters", timePerDot * 15, { left: "100%", ease: Linear.easeNone, onComplete: pacmanReverse }, "pacman")
+        .to(".pacman .top", timePerDot / 2, { rotation: -45, repeat: 21, yoyo: true }, "pacman")
+        .to(".pacman .bottom", timePerDot / 2, { rotation: 45, repeat: 21, yoyo: true }, "pacman")
+//        .to(".monster", timePerDot / 2, { y: "-=3px", repeat: 31, yoyo: true }, "pacman")
         .to(".dot1", .1, { opacity: 0, delay: timePerDot }, "pacman")
         .to(".dot2", .1, { opacity: 0, delay: timePerDot * 2}, "pacman")
         .to(".dot3", .1, { opacity: 0, delay: timePerDot * 3}, "pacman")
@@ -20,7 +23,27 @@ $(function () {
         .to(".dot8", .1, { opacity: 0, delay: timePerDot * 8}, "pacman")
         .to(".dot9", .1, { opacity: 0, delay: timePerDot * 9}, "pacman")
         .to(".dot10", .1, { opacity: 0, delay: timePerDot * 10}, "pacman")
+        .to(monsterAfterRule, timePerDot / 2, { cssRule: { top: "-=7px", repeat: 30, yoyo: true }});
+
+    function pacmanReverse() {
+        $(".monster").addClass("vulnerable");
+        $(".pacman").addClass("reverse");
+
+        var pacmanReverseTimeline = new TimelineMax({delay: 1});
+        pacmanReverseTimeline.addLabel("pacmanReverse")
+            .to(".characters", timePerDot * 15 / 1.5, { left: "0", ease: Linear.easeNone}, "pacmanReverse")
+            .to(".pacman .top", timePerDot / 2 / 1.5, { rotation: 45, repeat: 30, yoyo: true }, "pacmanReverse")
+            .to(".pacman .bottom", timePerDot / 2 / 1.5, { rotation: -45, repeat: 30, yoyo: true }, "pacmanReverse")
+            .to(".monster .mouth", timePerDot / 2 / 1.5, {
+                x: "-=1",
+                y: "+=1",
+                repeat: 30,
+                yoyo: true
+            }, "pacmanReverse")
+            .play();
+    }
 
     mainTimeline.add(pacmanTimeline, 0);
     mainTimeline.play();
 });
+
